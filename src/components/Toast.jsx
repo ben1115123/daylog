9a078afff18264c7466b706bf7ddeb6459e1ea8b
ben1115123 <tanbenjamin30@gmail.com>
2026-07-17 09:37:37 +0800

    feat: add optional undo action button to Toast

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,5 +1,18 @@
 import './Toast.css'
 
-export default function Toast({ msg, type = 'success' }) {
-  return <div className={`toast toast-${type}`}>{msg}</div>
+export default function Toast({ msg, type = 'success', action }) {
+  return (
+    <div className={`toast toast-${type}`}>
+      <span className="toast-msg">{msg}</span>
+      {action && (
+        <button
+          type="button"
+          className="toast-action"
+          onClick={(e) => { e.stopPropagation(); action.onClick() }}
+        >
+          {action.label}
+        </button>
+      )}
+    </div>
+  )
 }
